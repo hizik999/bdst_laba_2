bda3b73129b70220267ededdf3c5d10995f38e57
MalyshevAM <com.hizik@gmail.com>
2024-10-09 11:22:07 +0300

    добавил метрику gmv360d_new

--- main.sql
+++ main.sql
@@ -1,3 +1,4 @@
+/*
 drop table if exists dbo.orders;
 create table dbo.orders(
 	id serial primary key, 
@@ -7,17 +8,6 @@ create table dbo.orders(
 	order_cost real,
 	success_order_flg bool
 );
-/*
-drop table if exists dbo.analytics;
-create table dbo.analytics(
-	id serial primary key,
-	date date,
-	gmv360d_new real,
-	gmv360d_reactivated real,
-	users_count_new integer,
-	users_count_reactivated integer
-);
-*/
 
 
 INSERT INTO dbo.orders (user_id, order_id, order_time, order_cost, success_order_flg)
@@ -30,45 +20,48 @@ SELECT
 FROM 
     generate_series(1, 1000);
 
-
+*/
 drop table if exists dbo.analytics;
 
 WITH first_orders AS (
     SELECT 
     	user_id, 
-    	MIN(to_timestamp(order_time)) AS first_order_time
+    	order_cost,
+    	to_timestamp(order_time)::date AS first_order_time,
+    	ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY order_time) AS rn
     FROM dbo.orders
-    GROUP BY user_id
 ),
 middleware_analytics as (
 	SELECT 
 	    to_timestamp(o.order_time)::date AS date,
-	    0 AS gmv360d_new,
+	    SUM(fo.order_cost) AS gmv360d_new,
 	    0 AS gmv360d_reactivated,
 	    COUNT(fo.user_id) AS users_count_new,
-	    0 AS users_count_reactivated
+	    0 AS users_count_reactivated,
+	    row_number () over (partition by to_timestamp(o.order_time)::date order by to_timestamp(o.order_time)) as rn
 	FROM dbo.orders o
 	LEFT JOIN first_orders fo 
-	    ON fo.first_order_time = to_timestamp(o.order_time)
+	    ON fo.first_order_time = to_timestamp(o.order_time)::date
+	where fo.rn = 1
 	GROUP BY to_timestamp(o.order_time)
-	ORDER BY to_timestamp(o.order_time) ASC
+	ORDER BY to_timestamp(o.order_time)ASC
 )
 
-
-
+--select * from first_orders where first_order_time = '2023-01-03' order by user_id, first_order_time
+--select * from middleware_analytics --where date = '2024-01-11'
 SELECT 
-    date,
-    SUM(gmv360d_new) AS gmv360d_new,
-    SUM(gmv360d_reactivated) AS gmv360d_reactivated,
-    SUM(users_count_new) AS users_count_new,
-    SUM(users_count_reactivated) AS users_count_reactivated
+    a.date,
+    a.gmv360d_new,
+    a.gmv360d_reactivated,
+    a.users_count_new,
+    a.users_count_reactivated
 into dbo.analytics
 FROM middleware_analytics a
-GROUP BY date
+where a.rn = 1
 order by date;
 
 select * from dbo.analytics a ;
 
 
 -- select user_id, count(user_id) from dbo.orders group by user_id having user_id = 'user_740' order by count(user_id) desc
--- select user_id, date(to_timestamp(order_time)) from dbo.orders o order by order_time
+-- select user_id, order_cost from dbo.orders o order by user_id
